d3876be211b05751f9b62df961d022f7f6d9440b
Varriount <clay.sweetser@gmail.com>
2013-09-09 13:57:28 -0400

    Updated Tests

--- colliberation/tests/test_document.py
+++ colliberation/tests/test_document.py
@@ -1,51 +1,182 @@
-from colliberation.document import Document
+from colliberation.document import Document, StateException
+from colliberation.interfaces import IDocument
+from zope.interface.verify import verifyClass, verifyObject
 from unittest import TestCase
+from mock import Mock
 
-TEST_NAME = 'testDoc'
-TEST_ID = 42
-TEST_VERSION = 0
-TEST_URL = 'file:\\\test.py'
-TEST_DATA = {'Owner': 'tester'}
-TEST_CONTENT = 'A quick red fox jumped over the brown lazy dog.'
-TEST_INSERTION_TEXT = 'yogurt'
+# Testing Parameters
+DOC_NAME = 'testDoc'
+DOC_ID = 42
+DOC_VERSION = 0
+DOC_URL = 'file:\\\test.py'
+DOC_DATA = {'Owner': 'tester'}
+DOC_CONTENT = 'A quick red fox jumped over the brown lazy dog.'
 
+CHANGED_CONTENT = 'A yogurt red fox jumped over the brown lazy dog.'
+CHANGED_TEXT = 'yogurt'
+DELETED_CONTENT = 'A  red fox jumped over the brown lazy dog.'
 
-class DocumentInitTest(TestCase):
+TEXT_START, TEXT_END = 2, 7
+INVALID_POSITIONS = (
+    (-3, TEXT_END),
+    (TEXT_START, 400),
+    (TEXT_END, TEXT_START)
+)
 
-    def test_document_init(self):
-        d = Document(name=TEST_NAME, content=TEST_CONTENT,
-                     version=TEST_VERSION, url=TEST_URL, metadata=TEST_DATA)
-        self.assertEqual(d.version, TEST_VERSION)
-        self.assertEqual(d.url, TEST_URL)
-        self.assertEqual(d.metadata, TEST_DATA)
-        self.assertEqual(d.content, TEST_CONTENT)
+SERIALIZER = Mock()
+UPDATE_DATA = Mock()
+UPDATE_DATA.configure_mock(
+    name=DOC_NAME,
+    id=DOC_ID,
+    version=DOC_VERSION,
+    url=DOC_URL,
+    metadata=DOC_DATA,
+    content=DOC_CONTENT
+)
 
 
 class DocumentTest(TestCase):
 
     def setUp(self):
-        self.document = Document(name=TEST_NAME, content=TEST_CONTENT,
-                                 version=TEST_VERSION, url=TEST_URL,
-                                 metadata=TEST_DATA)
+        self.document = Document(
+            id=DOC_ID,
+            name=DOC_NAME,
+            content=DOC_CONTENT,
+            version=DOC_VERSION,
+            url=DOC_URL,
+            metadata=DOC_DATA,
+        )
 
     def tearDown(self):
         self.document = None
 
-    def test_insert_text(self):
-        pass
-        # self.document.insert(TEST_INSERTION_TEXT)
+    def test_doc_interface_conformance(self):
+        """
+        Test that the document class we're testing conforms to the protocol.
+        """
+        self.assertTrue(verifyClass(IDocument, Document))
+        self.assertTrue(verifyObject(IDocument, self.document))
+
+    def test_change_text(self):
+        """
+        Test changing the document's content.
+        """
+        self.document.open()
+        self.document.change_text(
+            TEXT_START,
+            TEXT_END,
+            CHANGED_TEXT,
+        )
+        self.document.close()
+        self.assertNotEqual(self.document.content, DOC_CONTENT)
+        self.assertEqual(self.document.content, CHANGED_CONTENT)
+
+    def test_document_init(self):
+        """
+        Test that the document initializes correctly.
+        """
+        d = self.document
+        self.assertEqual(d.id, DOC_ID)
+        self.assertEqual(d.name, DOC_NAME)
+        self.assertEqual(d.content, DOC_CONTENT)
+        self.assertEqual(d.version, DOC_VERSION)
+        self.assertEqual(d.url, DOC_URL)
+        self.assertEqual(d.metadata, DOC_DATA)
+
+    def test_change_invalid_text(self):
+        """
+        Test rejection of invalid parameters when changing document text.
+        """
+        self.document.open()
+        for start, end in INVALID_POSITIONS:
+            self.assertRaises(
+                ValueError,
+                self.document.change_text,
+                start,
+                end,
+                CHANGED_TEXT,
+            )
+            self.assertEqual(self.document.content, DOC_CONTENT)
+        self.document.close()
 
     def test_delete_text(self):
-        pass
+        """
+        Test deletion of a document's content.
+        """
+        self.document.open()
+        self.document.delete_text(
+            TEXT_START,
+            TEXT_END
+        )
+        self.document.close()
+        self.assertNotEqual(self.document.content, DOC_CONTENT)
+        self.assertEqual(self.document.content, DELETED_CONTENT)
 
-    def test_commit(self):
-        pass
+    def test_delete_invalid_text(self):
+        """
+        Test rejection of invalid parameters when deleting document text.
+        """
+        self.document.open()
+        for start, end in INVALID_POSITIONS:
+            self.assertRaises(
+                ValueError,
+                self.document.delete_text,
+                start,
+                end
+            )
+            self.assertEqual(self.document.content, DOC_CONTENT)
+        self.document.close()
 
-    def test_revert(self):
-        pass
+    def test_document_open(self):
+        """
+        Test that a document opens correctly
+        """
+        self.document.open()
 
-    def test_available_versions(self):
-        pass
+    def test_document_double_open(self):
+        """
+        Test rejection of multiple opens
+        """
+        self.document.open()
+        self.assertRaises(StateException, self.document.open)
 
-    def test_retrieve_version_data(self):
-        pass
+    def test_document_close(self):
+        """
+        Test that a document closes correctly.
+        """
+        self.document.open()
+        self.document.close()
+
+    def test_document_double_close(self):
+        """
+        Test that a document rejects attempts to close multiple times.
+        """
+        self.assertRaises(StateException, self.document.close)
+        self.document.open()
+        self.document.close()
+        self.assertRaises(StateException, self.document.close)
+
+    def test_document_save(self):
+        """
+        Test that a document saves correctly.
+        Since this only notifies the document that it is being saved
+        (And the standard document doesn't need to do anything when saved)
+        This just calls the document's save function to make sure that it runs
+        without error.
+        """
+        self.document.save()
+
+    def test_document_update(self):
+        """
+        Test that a document updates correctly.
+        Make a mock name
+        """
+        self.document.update(UPDATE_DATA, include_id=True)
+        d = self.document
+
+        self.assertEqual(d.id, UPDATE_DATA.id)
+        self.assertEqual(d.name, UPDATE_DATA.name)
+        self.assertEqual(d.content, UPDATE_DATA.content)
+        self.assertEqual(d.version, UPDATE_DATA.version)
+        self.assertEqual(d.url, UPDATE_DATA.url)
+        self.assertEqual(d.metadata, UPDATE_DATA.metadata)
---
+++ colliberation/tests/test_workspace.py
@@ -0,0 +1,88 @@
+from unittest import TestCase
+from colliberation.interfaces import IWorkspace
+from colliberation.workspace import Workspace
+from colliberation.document import Document
+from mock import Mock
+from zope.interface.verify import verifyClass, verifyObject
+
+WORKSPACE_NAME = 'Test Workspace'
+WORKSPACE_ID = 46
+WORKSPACE_METADATA = {'temp': True, 'prefix': 'test'}
+
+MOCK_DOCUMENT = Mock(spec=Document())
+
+
+class WorkspaceTest(TestCase):
+
+    def setUp(self):
+        self.workspace = Workspace(
+            id=WORKSPACE_ID,
+            name=WORKSPACE_NAME,
+            metadata=WORKSPACE_METADATA
+        )
+
+    def tearDown(self):
+        self.workspace = None
+
+    def test_workspace_initialization(self):
+        assertEqual = self.assertEqual
+        workspace = self.workspace
+
+        assertEqual(workspace.name, WORKSPACE_NAME)
+        assertEqual(workspace.id, WORKSPACE_ID)
+        assertEqual(workspace.metadata, WORKSPACE_METADATA)
+        #assertEqual(workspace.documents, {})
+
+    def test_workspace_interface_conformance(self):
+        self.assertTrue(verifyClass(IWorkspace, Workspace))
+        self.assertTrue(verifyObject(IWorkspace, self.workspace))
+
+    def test_workspace_search(self):
+        """
+        Populate workspace with documents
+        Search for documents
+        Match given results to expected results.
+        """
+
+    def test_workspace_add_remove(self):
+        """
+        Add document to workspace.
+        Check for document existence
+        Remove Document from workspace
+        Check for Document non-existence
+        """
+        assertTrue = self.assertTrue
+        document_id = MOCK_DOCUMENT.id
+        workspace = self.workspace
+
+        workspace.add_document(MOCK_DOCUMENT)
+        results = workspace.search(document_id=document_id)
+        assertTrue(len(results) == 1)
+        assertTrue(results[1] == MOCK_DOCUMENT)
+
+        workspace.remove_document(MOCK_DOCUMENT)
+        results = workspace.search(document_id=document_id)
+        assertTrue(len(results) == 0)
+
+    def test_workspace_remove_invalid_document(self):
+        """
+        Remove document from workspace.
+        Check for error
+        """
+        self.assertRaises(
+            KeyError,
+            self.workspace.remove_document,
+            id=20
+        )
+
+    def test_workspace_save(self):
+        """
+        Call function
+        """
+        self.workspace.save()
+
+    def test_workspace_load(self):
+        """
+        Call function
+        """
+        self.workspace.load()
